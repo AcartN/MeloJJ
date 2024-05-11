64c01115c92abb78d0536e3d22aee5760eb43792
Nicolas Acart <nicolas.acart@protonmail.com>
2024-05-11 22:31:25 +0200

    feat: POC haze glassmorphism

--- app/src/main/java/fr/acart/melojj/ui/home/HomeScreen.kt
+++ app/src/main/java/fr/acart/melojj/ui/home/HomeScreen.kt
@@ -3,6 +3,9 @@ package fr.acart.melojj.ui.home
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
@@ -13,13 +16,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import dev.chrisbanes.haze.HazeState
+import dev.chrisbanes.haze.HazeStyle
+import dev.chrisbanes.haze.haze
+import dev.chrisbanes.haze.hazeChild
 import fr.acart.melojj.R
 import fr.acart.melojj.ui.base.UiScreen
 import fr.acart.melojj.ui.base.screenWithStatusBar
@@ -62,6 +71,58 @@ object HomeScreen : UiScreen<HomeViewModel> {
                             contentDescription = null,
                             tint = MeloJJTheme.colors.content.primary
                         )
+                        val hazeState = remember { HazeState() }
+                        Box(
+                            modifier = Modifier.fillMaxSize(),
+                        ) {
+                            Row(
+                                modifier = Modifier
+                                    .haze(hazeState),
+                            ) {
+                                Box(
+                                    modifier = Modifier
+                                        .weight(1f)
+                                        .fillMaxHeight()
+                                        .background(Color.Gray),
+                                )
+                                Box(
+                                    modifier = Modifier
+                                        .weight(1f)
+                                        .fillMaxHeight()
+                                        .background(Color.DarkGray),
+                                )
+                            }
+                            Text(
+                                modifier = Modifier
+                                    .haze(hazeState)
+                                    .align(Alignment.Center),
+                                text = "ceci est un texte long",
+                                color = Color.White,
+                                fontSize = 24.sp,
+                            )
+                            Box(
+                                modifier = Modifier
+                                    .align(Alignment.Center)
+                                    .hazeChild(
+                                        state = hazeState,
+                                        shape = MeloJJTheme.shapes.shapeCircle,
+                                        style = HazeStyle(
+                                            tint = Color.Black.copy(alpha = 0.05f),
+                                            blurRadius = 2.dp,
+                                            noiseFactor = 0.01f,
+                                        ),
+                                    )
+                                    .size(164.dp),
+                            ) {
+                                Text(
+                                    modifier = Modifier
+                                        .padding(top = 16.dp)
+                                        .align(Alignment.TopCenter),
+                                    text = "Center",
+                                    fontSize = 24.sp,
+                                )
+                            }
+                        }
                     }
                 }
 
