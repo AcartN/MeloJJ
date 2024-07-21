227a3c3f7bbc266188be4378b7d5e4f462f92253
Nicolas Acart <nicolas.acart@protonmail.com>
2024-07-21 03:55:52 +0200

    feat: setup koin

--- app/src/main/java/fr/acart/melojj/MainApplication.kt
+++ app/src/main/java/fr/acart/melojj/MainApplication.kt
@@ -5,14 +5,17 @@ import fr.acart.melojj.data.SpotifyAppRemoteProvider
 import fr.acart.melojj.data.dataModule
 import fr.acart.melojj.domain.domainModule
 import fr.acart.melojj.ui.uiModule
+import org.koin.core.KoinApplication
 import org.koin.core.context.startKoin
 import org.koin.dsl.module
 
 class MainApplication : Application() {
+
+    private lateinit var koinApp: KoinApplication
     override fun onCreate() {
         super.onCreate()
 
-        startKoin {
+        koinApp = startKoin {
             modules(
                 module {
                     single { SpotifyAppRemoteProvider(this@MainApplication) }
@@ -22,5 +25,10 @@ class MainApplication : Application() {
                 uiModule,
             )
         }
+    }
+
+    override fun onTerminate() {
+        super.onTerminate()
+        koinApp.koin.get<SpotifyAppRemoteProvider>().close()
     }
 }
